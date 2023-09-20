20491e12b676a5b7a3d4833c037aa35a4cb39375
virgil-hp <xing.virgil@gmail.com>
2023-09-20 14:16:46 +1000

    v-test

---
+++ fps_v.py
@@ -0,0 +1,30 @@
+import time
+import torch
+import cv2
+
+# 加载模型
+model = torch.hub.load('./', 'custom', path='runs/train/exp46/weights/best.pt', source='local')  # 加载自定义模型
+
+video = cv2.VideoCapture('fps-testv.mp4')
+
+num_frames = 100  # 测量FPS的帧数
+start_time = time.time()
+
+for i in range(num_frames):
+    ret, frame = video.read()
+    if not ret:
+        break
+
+    # 使用YOLOv5进行对象检测
+    results = model(frame)
+
+end_time = time.time()
+
+# 计算并打印FPS
+fps = num_frames / (end_time - start_time)
+print(f'FPS: {fps}')
+print(f'VFPS: {video.get(cv2.CAP_PROP_FPS)}')
+
+# 释放视频和销毁所有窗口
+video.release()
+cv2.destroyAllWindows()
